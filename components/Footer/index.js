c873f92afd4a1ae34bf5e7fcd84b7d22b86e193a
Hasib <raisulaminhasib@gmail.com>
2023-09-15 12:53:57 +0800

    removed drawer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,9 +1,5 @@
 import React from "react";
 import Socials from "../Socials";
-import { Drawer } from 'vaul';
-import Button from '@mui/material/Button';
-import DadJoke from "../Dad_jokes/dadJokes";
-import ReadMoreIcon from '@mui/icons-material/ReadMore';
 
 const Footer = ({}) => {
   return (
@@ -27,28 +23,6 @@ const Footer = ({}) => {
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
       <p>Raisul Amin Hasib © {new Date().getUTCFullYear()}</p>
       </h1>
-        <Drawer.Root shouldScaleBackground>
-            <Drawer.Trigger asChild>
-                <Button variant="outlined" size="small" endIcon={<ReadMoreIcon />}>Open Drawer</Button>
-            </Drawer.Trigger>
-            <Drawer.Portal>
-                <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-                <Drawer.Content className="bg-zinc-100 flex flex-col rounded-t-[10px] mt-24 fixed bottom-0 left-0 right-0">
-                    <div className="p-4 bg-white rounded-t-[10px] flex-1">
-                        <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-gray-300 mb-8" />
-                        <div className="max-w-md mx-auto">
-                            <Drawer.Title className="font-medium mb-4">Random Dad Jokes :p</Drawer.Title>
-                            <DadJoke/>
-                        </div>
-                    </div>
-                    <div className="p-4 bg-gray-100 border-t border-gray-200 mt-auto">
-                        <div className="flex gap-6 justify-center max-w-md mx-auto">
-                            <p>Raisul Amin Hasib © {new Date().getUTCFullYear()}</p>
-                        </div>
-                    </div>
-                </Drawer.Content>
-            </Drawer.Portal>
-        </Drawer.Root>
     </>
   );
 };
