73a0bf1e643ea427e952587ea07399ca6055c9c6
Hasib <raisulaminhasib@gmail.com>
2024-09-20 17:52:59 +0800

    fix nav bar button color in first load

--- components/Header/index.js
+++ components/Header/index.js
@@ -13,157 +13,160 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
 
   const { name, showBlog, showResume } = data;
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+    // Ensures the component is fully mounted before rendering theme-related items
+    useEffect(() => {
+        setMounted(true);
+    }, []);
 
-  return (
-    <>
-        <Popover className="block tablet:hidden mt-5">
-            {({ open }) => (
-                <>
-                    <div className="flex items-center justify-between p-2 laptop:p-0">
-                        <h1
-                            onClick={() => router.push("/")}
-                            className="font-medium p-2 laptop:p-0 link"
-                        >
-                            {name}.
-                        </h1>
+    // If the component is not mounted, return null to avoid incorrect rendering
+    if (!mounted) return null;
 
-                        <div className="flex items-center">
-                            {data.darkMode && (
-                                <Button
-                                    onClick={() =>
-                                        setTheme(theme === "dark" ? "light" : "dark")
-                                    }
-                                >
+    return (
+        <>
+            <Popover className="block tablet:hidden mt-5">
+                {({ open }) => (
+                    <>
+                        <div className="flex items-center justify-between p-2 laptop:p-0">
+                            <h1
+                                onClick={() => router.push("/")}
+                                className="font-medium p-2 laptop:p-0 link"
+                            >
+                                {name}.
+                            </h1>
+
+                            <div className="flex items-center">
+                                {data.darkMode && (
+                                    <Button
+                                        onClick={() =>
+                                            setTheme(theme === "dark" ? "light" : "dark")
+                                        }
+                                    >
+                                        <img
+                                            className="h-6"
+                                            src={`/images/${
+                                                theme === "dark" ? "moon.svg" : "sun.svg"
+                                            }`}
+                                        />
+                                    </Button>
+                                )}
+
+                                <Popover.Button>
                                     <img
-                                        className="h-6"
+                                        className="h-5"
                                         src={`/images/${
-                                            theme === "dark" ? "moon.svg" : "sun.svg"
+                                            !open
+                                                ? theme === "dark"
+                                                    ? "menu-white.svg"
+                                                    : "menu.svg"
+                                                : theme === "light"
+                                                    ? "cancel.svg"
+                                                    : "cancel-white.svg"
                                         }`}
-                                    ></img>
-                                </Button>
-                            )}
-
-                            <Popover.Button>
-                                <img
-                                    className="h-5"
-                                    src={`/images/${
-                                        !open
-                                            ? theme === "dark"
-                                                ? "menu-white.svg"
-                                                : "menu.svg"
-                                            : theme === "light"
-                                                ? "cancel.svg"
-                                                : "cancel-white.svg"
-                                    }`}
-                                ></img>
-                            </Popover.Button>
+                                    />
+                                </Popover.Button>
+                            </div>
                         </div>
-                    </div>
-                    <Popover.Panel
-                        className={`absolute right-0 z-10 w-11/12 p-4 ${
-                            theme === "dark" ? "bg-slate-800" : "bg-white"
-                        } shadow-md rounded-md`}
-                    >
-                        {!isBlog ? (
-                            <div className="grid grid-cols-1">
-                                <Button onClick={handleWorkScroll}>Projects</Button>
-                                <Button onClick={handleAboutScroll}>About</Button>
-                                {showBlog && (
-                                    <Button onClick={() => router.push("/blog")}>Blog</Button>
-                                )}
-                                {showResume && (
-                                    <Button onClick={() => router.push("/resume")}>
-                                        Resume
-                                    </Button>
-                                )}
+                        <Popover.Panel
+                            className={`absolute right-0 z-10 w-11/12 p-4 ${
+                                theme === "dark" ? "bg-slate-800" : "bg-white"
+                            } shadow-md rounded-md`}
+                        >
+                            {!isBlog ? (
+                                <div className="grid grid-cols-1">
+                                    <Button onClick={handleWorkScroll}>Projects</Button>
+                                    <Button onClick={handleAboutScroll}>About</Button>
+                                    {showBlog && (
+                                        <Button onClick={() => router.push("/blog")}>Blog</Button>
+                                    )}
+                                    {showResume && (
+                                        <Button onClick={() => router.push("/resume")}>
+                                            Resume
+                                        </Button>
+                                    )}
 
-                                <Button onClick={() => router.push("/resume")}>
-                                    Contact
-                                </Button>
-                            </div>
-                        ) : (
-                            <div className="grid grid-cols-1">
-                                <Button onClick={() => router.push("/")}>Home</Button>
-                                {showBlog && (
-                                    <Button onClick={() => router.push("/blog")}>Blog</Button>
-                                )}
-                                {showResume && (
-                                    <Button onClick={() => router.push("/resume")}>
-                                        Resume
-                                    </Button>
-                                )}
-                            </div>
-                        )}
-                    </Popover.Panel>
-                </>
-            )}
-        </Popover>
-        <div
-            className={`mt-10 hidden flex-row items-center justify-between ${
-                theme === "light" && "bg-white"
-            } dark:text-white top-0 z-10 tablet:flex`}
-        >
-            <h1
-                onClick={() => router.push("/")}
-                className="font-medium cursor-pointer mob:p-2 laptop:p-0"
+                                    <Button onClick={() => router.push("/resume")}>Contact</Button>
+                                </div>
+                            ) : (
+                                <div className="grid grid-cols-1">
+                                    <Button onClick={() => router.push("/")}>Home</Button>
+                                    {showBlog && (
+                                        <Button onClick={() => router.push("/blog")}>Blog</Button>
+                                    )}
+                                    {showResume && (
+                                        <Button onClick={() => router.push("/resume")}>
+                                            Resume
+                                        </Button>
+                                    )}
+                                </div>
+                            )}
+                        </Popover.Panel>
+                    </>
+                )}
+            </Popover>
+            <div
+                className={`mt-10 hidden flex-row items-center justify-between ${
+                    theme === "light" && "bg-white"
+                } dark:text-white top-0 z-10 tablet:flex`}
             >
-                {name}.
-            </h1>
-            {!isBlog ? (
-                <div className="flex">
-                    <Button onClick={handleWorkScroll}>Projects</Button>
-                    <Button onClick={handleAboutScroll}>About</Button>
-                    {showBlog && (
-                        <Button onClick={() => router.push("/blog")}>Blog</Button>
-                    )}
-                    {showResume && (
-                        <Button onClick={() => router.push("/resume")}>
-                            Resume
-                        </Button>
-                    )}
+                <h1
+                    onClick={() => router.push("/")}
+                    className="font-medium cursor-pointer mob:p-2 laptop:p-0"
+                >
+                    {name}.
+                </h1>
+                {!isBlog ? (
+                    <div className="flex">
+                        <Button onClick={handleWorkScroll}>Projects</Button>
+                        <Button onClick={handleAboutScroll}>About</Button>
+                        {showBlog && (
+                            <Button onClick={() => router.push("/blog")}>Blog</Button>
+                        )}
+                        {showResume && (
+                            <Button onClick={() => router.push("/resume")}>Resume</Button>
+                        )}
 
-                    {mounted && theme && data.darkMode && (
-                        <Button
-                            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-                        >
-                            <img
-                                className="h-6"
-                                src={`/images/${theme === "dark" ? "moon.svg" : "sun.svg"}`}
-                            ></img>
-                        </Button>
-                    )}
-                </div>
-            ) : (
-                <div className="flex">
-                    <Button onClick={() => router.push("/")}>Home</Button>
-                    {showBlog && (
-                        <Button onClick={() => router.push("/blog")}>Blog</Button>
-                    )}
-                    {showResume && (
-                        <Button onClick={() => router.push("/resume")}>
-                            Resume
-                        </Button>
-                    )}
+                        {mounted && theme && data.darkMode && (
+                            <Button
+                                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+                            >
+                                <img
+                                    className="h-6"
+                                    src={`/images/${
+                                        theme === "dark" ? "moon.svg" : "sun.svg"
+                                    }`}
+                                />
+                            </Button>
+                        )}
+                    </div>
+                ) : (
+                    <div className="flex">
+                        <Button onClick={() => router.push("/")}>Home</Button>
+                        {showBlog && (
+                            <Button onClick={() => router.push("/blog")}>Blog</Button>
+                        )}
+                        {showResume && (
+                            <Button onClick={() => router.push("/resume")}>Resume</Button>
+                        )}
 
-                    {mounted && theme && data.darkMode && (
-                        <Button
-                            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-                        >
-                            <img
-                                className="h-6"
-                                src={`/images/${theme === "dark" ? "moon.svg" : "sun.svg"}`}
-                            ></img>
-                        </Button>
-                    )}
-                </div>
-            )}
-        </div>
-    </>
-  );
+                        {mounted && theme && data.darkMode && (
+                            <Button
+                                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+                            >
+                                <img
+                                    className="h-6"
+                                    src={`/images/${
+                                        theme === "dark" ? "moon.svg" : "sun.svg"
+                                    }`}
+                                />
+                            </Button>
+                        )}
+                    </div>
+                )}
+            </div>
+        </>
+    );
 };
 
 export default Header;
+
 
